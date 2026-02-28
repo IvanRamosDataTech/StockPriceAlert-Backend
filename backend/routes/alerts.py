c72d8feff6d2741edda1161d6fa5397609f3429f
Ivan Ramos <ivanramos.datatech@gmail.com>
2026-02-28 14:27:04 -0600

    - Update alert endpoint

--- backend/routes/alerts.py
+++ backend/routes/alerts.py
@@ -97,7 +97,51 @@ def unset_alert(alert_id):
                 return jsonify({"error": f"No alert found with ID {alert_id}"}), 404
     
             session.delete(alert)
-            return jsonify({"message": f"Alert {alert.alert_type} for asset {alert.asset.ticker} successfully deleted"})
+            return jsonify({"message": f"Alert {alert.alert_type} {alert.price_threshold if alert.price_threshold is not None else ''} for asset {alert.asset.ticker} successfully deleted"})
     except Exception as e:
         logger.error(f"Error deleting alert: {e}")
         return jsonify({"error": "Failed to delete alert"}), 500
+    
+
+@alerts_blueprint.route('/<int:alert_id>', methods=['PATCH'])
+def update_alert(alert_id):
+    """
+    Endpoint to update an existing alert by its ID
+    Expects JSON body with keys:
+        {"alert_type": [string] - Accepted values: MonthMinimum | MonthMaximum | PriceBelow | PriceAbove,
+         "target_price": [float] - Threshold price (required) in case alert is of type PriceBelow or PriceAbove
+    }
+    """
+    logger.info(f"/api/alerts/{alert_id} PATCH route called")
+    
+    data = request.get_json()
+    
+    alert_type = data.get('alert_type')
+    target_price = data.get('target_price')
+    
+    # Data type validations
+    if not isinstance(alert_type, str):
+        return jsonify({"error": "alert_type must be a string"}), 400
+    
+    if not isinstance(target_price, (int, float)) and target_price is not None:
+        return jsonify({"error": "target_price must be a number"}), 400
+
+    if alert_type not in ["MonthMinimum", "MonthMaximum", "PriceBelow", "PriceAbove"]:
+        return jsonify({"error": "Invalid alert_type. Accepted values are: MonthMinimum, MonthMaximum, PriceBelow, PriceAbove"}), 400
+
+    if alert_type in ["PriceBelow", "PriceAbove"] and target_price is None:
+        return jsonify({"error": "target_price is required for PriceBelow and PriceAbove alert types"}), 400
+    
+    try:
+        with get_db_session() as session:
+            alert = Alert.query.get(alert_id)
+            if not alert:
+                return jsonify({"error": f"No alert found with ID {alert_id}"}), 404
+            
+            alert.alert_type = alert_type
+            alert.price_threshold = target_price
+            session.add(alert)
+            return jsonify({"message": f"Alert {alert.alert_type} {alert.price_threshold if alert.price_threshold is not None else ''} for asset {alert.asset.ticker} updated successfully"})
+    except Exception as e:
+        logger.error(f"Error updating alert: {e}")
+        return jsonify({"error": "Failed to update alert"}), 500
