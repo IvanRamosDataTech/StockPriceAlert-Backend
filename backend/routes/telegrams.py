51cdb9e4bf25972a6d5981f3b5500b4d3c838003
Ivan Ramos <ivanramos.datatech@gmail.com>
2026-03-24 07:36:34 -0600

    deserialize message

--- backend/routes/telegrams.py
+++ backend/routes/telegrams.py
@@ -7,6 +7,12 @@ logger = logging.getLogger(__name__)
 
 telegrams_blueprint = Blueprint('telegrams', __name__, url_prefix='/telegram')
 
+def _process_command(command):
+    logger.info(f"Processing command: {command}")
+    # Implement command processing logic here
+    TelegramService.send_message(current_app, f"Processed command: {command}")
+
+
 @telegrams_blueprint.route('/', methods=["POST"])
 def send_telegram_message():
     logger.info("Telegram message route called")
@@ -15,7 +21,12 @@ def send_telegram_message():
     #     return jsonify({"error": "json input needs a message property "}), 400
     try:
         logger.info(f"Received message to send: {data}")
-        TelegramService.send_message(current_app, str(data))
+        message_obj = data.get("message", None)
+        if message_obj and "entities" in message_obj and message_obj["entities"][0]["type"] == "bot_command":
+            text = message_obj["text"]
+            _process_command(text)
+        else:
+            TelegramService.send_message(current_app, "I didn't get that. Please use /help to see available commands.")
         return jsonify(data), 200
     except Exception as e:
         logger.error(f"Error sending message: {e}")
